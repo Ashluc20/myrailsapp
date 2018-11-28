c7c8b2dd4bd329d1bb92c2742ca10f028807b1f7
Ashluc20 <ashleylucero20@gmail.com>
2018-11-27 21:03:24 -0700

    orders controller attempt 1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,23 @@
 class OrdersController < ApplicationController
   def index
+    @order = Order.all
   end
 
   def show
+    @order = Order.all
   end
 
   def new
+    @order = Order.new
   end
 
   def create
   end
 
   def destroy
+    @order.destroy
+    respond_to do |format|
+      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
+      format.json { head :no_content }
   end
 end
