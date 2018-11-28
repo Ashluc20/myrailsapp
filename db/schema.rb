835287feef3b7aaa6f53efb8353b5c92f7d98989
Ashluc20 <ashleylucero20@gmail.com>
2018-11-27 19:14:51 -0700

    added price migration with decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_030921) do
+ActiveRecord::Schema.define(version: 2018_11_28_014501) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.float "total"
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_11_14_030921) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "price"
   end
 
 end
