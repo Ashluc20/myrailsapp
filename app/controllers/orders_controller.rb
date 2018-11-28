70e2ce0892c1cf33a75b8e085646fdf50bfcfca1
Ashluc20 <ashleylucero20@gmail.com>
2018-11-27 22:10:32 -0700

    added views to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,17 @@
 class OrdersController < ApplicationController
   def index
-    @order = Order.all
   end
 
   def show
-    @order = Order.all
   end
 
   def new
-    @order = Order.new
   end
 
   def create
   end
 
   def destroy
-    @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
+
   end
 end
