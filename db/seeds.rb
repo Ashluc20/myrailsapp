a8b8330382145f83110684d68ceb7aba3b9293f5
Ashluc20 <ashleylucero20@gmail.com>
2018-12-10 22:12:40 -0700

    new images in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Product.delete_all
-Product.create(name: "Product 1", description: "Description for Product 1", image_url: "https://pixabay.com/get/ea32b80c21f71c22d2524518b7444795ea76e5d004b0144591f4c971afefbc_340.jpg", price: 100)
-Product.create(name: "Product 2", description: "Description for Product 2", image_url: "https://pixabay.com/get/ef36b10720e90021d85a5854ee4d459fe270e7c818b4154591f8c971a5e4_340.jpg", price: 200)
-Product.create(name: "Product 3", description: "Description for Product 3", image_url: "https://pixabay.com/get/ea32b00f2cf3023ed1584d05fb1d4e97e07ee3d21cac104491f4c571afe5b7b1_340.jpg", price: 300)
-Product.create(name: "Product 4", description: "Description for Product 4", image_url: "https://farm3.staticflickr.com/2690/4411460045_d7491ed4ea.jpg", price: 400)
-Product.create(name: "Product 5", description: "Description for Product 5", image_url: "https://farm3.staticflickr.com/2709/4396687745_6d85772f0f.jpg", price: 500)
+Product.create(name: "Product 1", description: "Description for Product 1", image_url: "https://images.gr-assets.com/books/1452634932l/28525930.jpg", price: 0)
+Product.create(name: "Product 2", description: "Description for Product 2", image_url: "https://images-na.ssl-images-amazon.com/images/I/51GIQp7spHL._SX331_BO1,204,203,200_.jpg", price: 10)
+Product.create(name: "Product 3", description: "Description for Product 3", image_url: "https://pixabay.com/get/ea32b00f2cf3023ed1584d05fb1d4e97e07ee3d21cac104491f4c571afe5b7b1_340.jpg", price: 0)
+Product.create(name: "Product 4", description: "Description for Product 4", image_url: "https://image.shutterstock.com/z/stock-vector-stay-tuned-coming-soon-text-on-geometric-polygonal-background-499604734.jpg", price: 0)
+Product.create(name: "Product 5", description: "Description for Product 5", image_url: "https://farm3.staticflickr.com/2709/4396687745_6d85772f0f.jpg", price: 0)
 
 Order.delete_all
 Order.create(user_id: 1, product_id: Product.first.id, total: 100)
