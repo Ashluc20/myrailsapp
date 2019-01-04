d090128229f0a2cdcf70bd6b1a0d06189148f5ba
Ashluc20 <ashleylucero20@gmail.com>
2019-01-03 23:38:23 -0700

    fix search functionality and implement case insensitive searching on production

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,12 @@
 class Product < ApplicationRecord
   has_many :orders
-end
 
-def self.search(search_term)
-  Product.where("name LIKE ?", "%#{search_term}%")
+
+  def self.search(search_term)
+    if Rails.env.production?
+      Product.where("name iLIKE ?", "%#{search_term}%")
+    else
+      Product.where("name LIKE ?", "%#{search_term}%")
+    end
+  end
 end
