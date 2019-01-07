097ba6de263ad8791b54030d621e93cbf7804a78
Ashluc20 <ashleylucero20@gmail.com>
2019-01-06 19:26:03 -0700

    fixed error moved code to be inside for mailer to work

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -3,16 +3,16 @@ class SimplePagesController < ApplicationController
   end
 
   def landing_page
-    @products = Product.limit(3)
-  end
-end
+      @products = Product.limit(3)
 
-def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  ActionMailer::Base.mail(from: @email,
-      to: 'ashleylucero20@gmail.com',
-      subject: "A new contact form message from #{@name}",
-      body: @message).deliver_now
+
+  def thank_you
+      @name = params[:name]
+      @email = params[:email]
+      @message = params[:message]
+      ActionMailer::Base.mail(from: @email,
+        to: 'ashleylucero20@gmail.com',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
+  end
 end
