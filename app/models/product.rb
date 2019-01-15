85a8edc514e048aa49038b78aad8d63ca71d0d7f
Ashluc20 <ashleylucero20@gmail.com>
2019-01-14 20:58:42 -0700

    5.8 assignment rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   has_many :orders
   has_many :comments
 
+  Client.average("orders_count")
+
 
   def self.search(search_term)
     if Rails.env.production?
@@ -14,4 +16,8 @@ class Product < ApplicationRecord
   def highest_rating_comment
   comments.rating_desc.first
   end
+end
+
+def average_rating
+  comments.average(:rating).to_f
 end
