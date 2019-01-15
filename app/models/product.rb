8cc06acf45b9e4574c52a9ab30854f00b1dbd70e
Ashluc20 <ashleylucero20@gmail.com>
2019-01-14 21:54:35 -0700

    error fixes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,6 @@ class Product < ApplicationRecord
   has_many :orders
   has_many :comments
 
-  Client.average("orders_count")
-
 
   def self.search(search_term)
     if Rails.env.production?
@@ -14,10 +12,10 @@ class Product < ApplicationRecord
   end
 
   def highest_rating_comment
-  comments.rating_desc.first
+    comments.rating_desc.first
   end
-end
 
-def average_rating
-  comments.average(:rating).to_f
+  def average_rating
+    comments.average(:rating).to_f
+  end
 end
