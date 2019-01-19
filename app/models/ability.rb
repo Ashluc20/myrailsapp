fb2dcb0079460d2e23a94ef05209afe7b773fdf3
Ashluc20 <ashleylucero20@gmail.com>
2019-01-18 17:46:12 -0700

    pushing to github to view errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,5 +5,10 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
     can :read, :all
+    if user.admin?
+    can :manage, :all
+    else
+    can :read, :all
+    end
   end
 end
