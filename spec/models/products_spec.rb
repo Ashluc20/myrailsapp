19a8a8a221b3d31b481686f2016d603929fbd47c
Ashluc20 <ashleylucero20@gmail.com>
2019-01-26 20:26:19 -0700

    fixed rspec test in product rspec

--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -3,25 +3,23 @@ require 'rails_helper'
 describe Product do
   context "when the product has comments" do
     let(:product) { Product.create!(name: "race bike") }
-    it "should return race bike when product has comments" do
-      expect(Product.description).to eq "race bike"
-    end
-  end
+    let(:product) {Product.new!(description:"Nice Bike") }
 
-  context "average product rating" do
-    let(:user) {User.create!(email: "test@gmail.com", password: "password")}
     before do
       product.comments.create!(rating: 1, user: user, body: "Awful bike!")
       product.comments.create!(rating: 3, user: user, body: "Ok bike!")
       product.comments.create!(rating: 5, user: user, body: "Great bike!")
+    end
+
+    it "should return race bike when product has comments" do
+      expect(Product.description).to eq "race bike"
+    end
+
     it "returns the average rating of all comments" do
       expects(Products.average_rating).to eq "3"
     end
-  end
 
-  context "is not valid without a name" do
-    let(:product) {Product.new!(description:"Nice Bike") }
-    it "returns not vaild without name" do
+    it "is not vaild without name" do
       expect(Product.new(description: "Nice bike")).not_to be_valid
     end
   end
