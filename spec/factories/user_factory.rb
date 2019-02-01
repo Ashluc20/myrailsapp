aec1841f20d36eb0ff44bc5e2da3e720b4f12062
Ashluc20 <ashleylucero20@gmail.com>
2019-01-31 20:03:20 -0700

    resolved 6.4 config/environments errors

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   sequence(:email) { |n| "user#{n}@example.com" }
   factory :admin, class: User do
     email
-    password ...
-    first_name "Peter"
-    last_name "Example"
-    admin false
+    password {"abc123abc123"}
+    first_name {"Peter"}
+    last_name {"Example"}
+    admin {false}
   end
 end
