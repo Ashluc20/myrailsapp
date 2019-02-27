be081b610f5e17e1e074e3f14c1885feed4088df
Ashluc20 <ashleylucero20@gmail.com>
2019-02-26 22:24:42 -0700

    changed routes.rb file git products

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  get 'products'
+  get 'simple_pages/products'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'simple_pages#index'
   resources :orders, only: [:index, :show, :create, :destroy]
